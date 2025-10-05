67eea80e335f63bcc678b230f2ce6c66ebda1431
Student-Aaditya <aadityakuamr518@gmail.com>
2025-10-05 23:59:34 +0530

    adding blog.jsx file data

--- Frontend/frontend/src/Page/Blog.jsx
+++ Frontend/frontend/src/Page/Blog.jsx
@@ -3,9 +3,15 @@ function Blog() {
   return (
     <>
     <Header textColor={"text-black"} textFamily={"font-mono"}></Header>
-    <h1>Blog </h1>
-    <div className="bg-[url('cricket.jpeg')] bg-cover bg-center w-280 h-125 ml-4 rounded-3xl ">
-
+ 
+    <div className="bg-[url('cricket.jpeg')] bg-cover bg-center w-250 h-125 ml-55 p-5 text-white  rounded-3xl ">
+       <h1 className="w-250 m-auto items-center text-2xl mt-5">Welcome to our blog, a place where ideas, stories, and knowledge come together. Here,
+       we share insights, tips, and experiences that inspire, inform, and empower our readers.
+        Whether you're looking for practical advice, the latest trends, or a spark of creativity,
+         our articles are crafted to provide value and engage your mind. We believe that every story 
+         has the power to teach, every idea can
+       ignite change, and every reader can find something meaningful to take away. Join us on this 
+       journey of discovery, learning, and inspiration. </h1>
     </div>
     </>
   );
